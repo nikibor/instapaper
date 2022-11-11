91c1ff2b06f5d0ed54cdc7b7965d93d13f8cd7af
nikita.borgolov <nikita.borgolov@ringcentral.com>
2022-11-12 00:35:25 +0100

    Get Rust actual Rust version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::anyhow;
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let html = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+    let document = Html::parse_document(&html);
+
+    let selector = Selector::parse("body > main > header > div > div.w-30-l.flex-column.pl0-l.pr0-l.pl3.pr3 > p > a").unwrap();
+
+    for element in document.select(&selector) {
+        println!("{}", element.inner_html());
+    }
+    Ok(())
 }
